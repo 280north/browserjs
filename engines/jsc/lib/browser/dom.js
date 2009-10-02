da4e5cd2205490a7bc7c77c50d22fff3d18bf8ea
Tom Robinson <tom@280north.com>
2009-10-01 18:50:21 -0700

    Cleanup dom module for jsc/browser

--- engines/jsc/lib/browser/dom.js
+++ engines/jsc/lib/browser/dom.js
@@ -1,27 +1,18 @@
+exports.Node = global.Node;
+exports.Element = global.Element;
+exports.Document = global.Document;
+
+exports.NodeList = global.NodeList;
+
+exports.DOMParser = global.DOMParser;
+exports.XMLSerializer = global.XMLSerializer;
+exports.XPathResult = global.XPathResult;
 
 exports.createDocument = function(/*String*/ namespaceURI, /*String*/ qualifiedName, /*DocumentType*/ doctype)  {
     return global.document.implementation.createDocument(namespaceURI, qualifiedName, doctype || null);
 }
 
-exports.XMLSerializer = global.XMLSerializer;
-
-// DOMParser
-exports.DOMParser = global.DOMParser;
-
-var validContentTypes = { "text/xml" : true, "application/xml" : true, "application/xhtml+xml" : true };
-
-// FIXME: wrap Document so we can add these:
-
-exports.createExpression = function(/*String*/ xpathText, /*Function*/ namespaceURLMapper) {
-    return (new XPathEvaluator).createExpression(xpathText, namespaceURLMapper).
-}
-
 exports.evaluate = function(/*String*/ xpathText, /*Node*/ contextNode, /*Function*/ namespaceURLMapper, /*short*/ resultType, /*XPathResult*/ result) {
-    return exports.createExpression(xpathText, namespaceURLMapper).evaluate(contextNode, resultType, result);
+    var doc = contextNode instanceof exports.Document ? contextNode : contextNode.ownerDocument;
+    return doc.evaluate(xpathText, contextNode, namespaceURLMapper, resultType, result || null);
 }
-
-exports.Node = global.Node;
-exports.Node = global.Document;
-exports.Element = global.Element;
-exports.NodeList = global.NodeList;
-exports.Document = global.Document;
